4388e7a497363b14e1181db6579eecfcd3c48726
Gwendal20 <gsaget@et.esiea.fr>
2024-11-12 23:08:05 +0100

    fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.cronet.embedded)
     implementation(libs.books)
+    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
